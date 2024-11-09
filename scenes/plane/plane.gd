8f39e2e5a9b3f930e1e4c774dec73192407c8716
Thomas Winterfield <twinterfield68@gmail.com>
2024-11-09 03:09:04 -0500

    first commit, plane physics and game scene

---
+++ scenes/plane/plane.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimSprite
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	print(velocity)
+	
+	# acceleration due to gravity
+	velocity.y += GRAVITY * delta
+	fly()
+	move_and_slide()
+	if is_on_floor():
+		die()
+
+func fly() -> void:
+	# if fly is pressed, give it upward velocity vector
+	if Input.is_action_just_pressed("fly"):
+		velocity.y = POWER 
+		anim_player.play("power")
+
+func die() -> void:
+	anim_sprite.stop()
+	set_physics_process(false)
